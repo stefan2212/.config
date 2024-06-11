276b6f25a2e57cac29cc166e209b6af10c2ac5c2
Stefan Cernescu <stefan.cernescu@bp.com>
2024-06-11 19:11:52 +0300

    First Config. Only nvim and iterm2

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+require 'stefan.core'
+require 'stefan.lazy'
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
